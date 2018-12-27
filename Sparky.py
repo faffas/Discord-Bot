91d6cbcb0a23afd7693759a9b9aafdb9136c91fc
Atharva Kulkarni <38763219+IronVenom@users.noreply.github.com>
2018-12-27 12:34:12 +0530

    Update Sparky.py

--- Sparky.py
+++ Sparky.py
@@ -3,8 +3,10 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import time
+import imdb
 import os
 
+ia=imdb.IMDb()
 Client = discord.Client()
 client = commands.Bot(command_prefix="!")
 
@@ -12,7 +14,7 @@ client = commands.Bot(command_prefix="!")
 async def on_ready():
 	print("Badum tss, I am ready!")
   
-#Greeting someone when the user says !hello,!yo and !wazz poppin?
+#Greeting someone when the user says !hello,!yo and !wazz poppin/ Movies Bot integration.
 @client.event
 async def on_message(message):
 	if message.content.upper().startswith('!HELLO'):
@@ -24,6 +26,17 @@ async def on_message(message):
 	if message.content.upper().startswith('!WAZZ POPPIN?'):
 		userID = message.author.id 
 		await client.send_message(message.channel,"Not much, <@%s>!" % (userID))
+	if message.content.upper().startswith('MOVIE!'):
+		userID = message.author.id 
+		args = message.content.split(" ")
+		moviename=" ".join(args[1:])
+		movie=ia.search_movie(moviename)
+		movie1=movie[0]
+		movieid=ia.get_imdbID(movie1)
+		movieinfo=ia.get_movie(movieid)
+		plot=movieinfo['plot'][0]
+		await client.send_message(message.channel,plot)
+
 
 #Introduction of a new user. Note that in asyncio the ids are strings.	
 @client.event
