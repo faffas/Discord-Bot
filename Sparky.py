9a144ffda37b6910fd819fe34a3da57fbcd6c8f3
Atharva Kulkarni <38763219+IronVenom@users.noreply.github.com>
2019-01-23 01:44:28 +0530

    Update Sparky.py

--- Sparky.py
+++ Sparky.py
@@ -5,6 +5,7 @@ import asyncio
 import time
 import imdb
 import os
+import random
 import wikipedia as wk
 from newsapi import NewsApiClient
 
@@ -84,9 +85,68 @@ async def on_message(message):
 		embed.add_field(name='help!',value='Gives the list of commands.',inline=False)
 		embed.add_field(name='roles!',value='Gives all the roles present in the server.',inline=False)
 		embed.add_field(name='info!',value='Gives server info.',inline=False)
+		embed.add_field(name='coin! type heads or tails',value='Make Sparky toss a coin and see if you win',inline=False)
+		embed.add_field(name='slot!',value='Test your luck on Sparky\'s slot machine!',inline=False)
 		embed.add_field(name='wiki! what you want to search',value='Gives brief summary from Wikipedia of the queried item.',inline=False)
 		embed.add_field(name='movie! name of Movie / TV Series /  Video Game',value='Gives the plot summary of the Movie/ TV series / Video Game.',inline=False)
 		embed.add_field(name='hello! / yo! / wazz poppin!',value='Sparky says hi to you!', inline=False)
+		await client.send_message(message.channel,embed=embed)
+	
+	#Coin Flip Game
+	if message.content.upper().startswith('COIN!'):
+		args=message.content.split(" ")
+		result_list=["Heads","Tails"]
+		choice=random.randint(0,1)
+		if args[1].upper()==result_list[choice].upper():
+			result="{} it is! You win!".format(result_list[choice])
+			embed=discord.Embed(title='Coin Flip',description=result,colour=discord.Colour.teal())
+			await client.send_message(message.channel,embed=embed)
+		else:
+			result=" Uh oh, its {}! Better luck next time!".format(result_list[choice])
+			embed=discord.Embed(title='Coin Flip',description=result,colour=discord.Colour.teal())
+			await client.send_message(message.channel,embed=embed)
+
+	#Slot Machine Game
+	if message.content.upper().startswith('SLOT!'):
+		result_list=[':apple:',':pear:',':tangerine:']
+		result_list2=[':grapes:',':strawberry:',':cherries:']
+		result_list3=[':hotdog:',':icecream:',':taco:']
+		choice1=random.randint(0,2)
+		choice2=random.randint(0,2)
+		choice3=random.randint(0,2)
+		e11=result_list[choice1]
+		e12=result_list[choice2]
+		e13=result_list[choice3]
+		e21=result_list2[choice1]
+		e22=result_list2[choice2]
+		e23=result_list2[choice3]
+		e31=result_list3[choice1]
+		e32=result_list3[choice2]
+		e33=result_list3[choice3]
+		result=e11+" | "+e12+" | "+e13+" | "+"\n"+e21+" | "+e22+" | "+e23+" | "+"\n"+e31+" | "+e32+" | "+e33+" | "
+		row1=False
+		row2=False
+		row3=False
+		row_count=0
+		if (e11==e12) and (e12==e13) and (e13==e11):
+			row1=True
+			row_count+=1
+		if (e21==e22) and (e22==e23) and (e23==e21):
+			row2=True
+			row_count+=1
+		if (e31==e32) and (e32==e33) and (e33==e31):
+			row3=True
+			row_count+=1
+		if row_count==0:
+			res_mes="Better luck next time!"
+		if row_count==1:
+			res_mes="You got 1 row! Nice work!"
+		if row_count==2:
+			res_mes="You got 2 rows! Awesome!"
+		if row_count==3:
+			res_mes="Hattrick!"
+		embed=discord.Embed(title='Slot Machine',description=result,colour=discord.Colour.teal())
+		embed.add_field(name='Result',value=res_mes, inline=False)
 		await client.send_message(message.channel,embed=embed)
 
 
